4cd1265c82ffbc909101911056ea874cedd6724e
Sougata Patra <sougatapatra23356@gmail.com>
2026-01-21 23:31:01 +0530

    DB connection successful , basic setup for the express server , updated utils .

---
+++ src/utils/api_error.js
@@ -0,0 +1,20 @@
+class api_error extends Error {
+  constructor(
+    statusCode,
+    message = "Something is Wrong",
+    errors = [],
+    stack = "",
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
---
+++ src/utils/api_response.js
@@ -0,0 +1,12 @@
+// Api_Response
+
+class api_response {
+  constructor(statusCode, data, message = "Success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
+
+export { api_response };
